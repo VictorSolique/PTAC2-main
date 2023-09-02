d76c6c91af6d5cfc20e06c0e534be0965c6eb060
Victor de Souza Solique <99771825+VictorSolique@users.noreply.github.com>
2023-09-01 22:58:12 -0400

    commit dia 01/09

--- src/todo/Todo.jsx
+++ src/todo/Todo.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
 
+
 export default function Todo() {
     const[lista, setLista] = useState([]);
     const[atividade, setAtiv] = useState("");
@@ -8,9 +9,10 @@ export default function Todo() {
 
     const salvar = (e) => {
         e.preventDefault();
+        if(atividade === "") return;
         setLista([...lista, {
             id: id,
-            atividade: atividade
+            atividade: atividade.charAt(0).toUpperCase() + atividade.slice(1)
         }]);
         setId(id+1);
         setAtiv("");
@@ -21,20 +23,42 @@ export default function Todo() {
         setLista(filtra);
         console.log(filtra, listaAux, id, "Está rondando");  
     }
+    function removeTudo(e) {
+        e.preventDefault();
+        return setLista([]);
+    }
+    
+
     return (
-        <div>
-            <h1>Todo</h1>
-            <Link to="/">Link do todo</Link>
-            <form onSubmit={salvar}>
-                <input type="text" value={atividade} onChange={(e) => setAtiv(e.target.value)} />
-                <button>ADD</button>
+        <div class="container bg-cinza pt-4">
+            <nav aria-label="breadcrumb">
+                <ol class="breadcrumb">
+                    <li class="breadcrumb-item"><Link to="/">Home</Link></li>
+                    <li class="breadcrumb-item active" aria-current="page">toDo</li>
+                </ol>
+            </nav>
+            
+            <h1 class="text-start">Lista de Atividades</h1>
+            <form onSubmit={salvar} class="input-group mb-3">
+                <input type="text" class="form-control" placeholder="Adicione uma comida na lista" aria-describedby="button-addon2" value={atividade} onChange={(e) => setAtiv(e.target.value)} />
+                <button class="btn btn-success">ADICIONAR</button>
             </form>
-            {lista.map((atividade) => (
-                <div key={atividade.id} class="ms-2">
-                    <span> - {atividade.atividade}</span>
-                    <button type="button" class="btn-close m-2" aria-label="Close" onClick={() => remove(atividade.id)}></button>
+            <div class="container">
+                
+                <div class="row">    
+                <a onClick={removeTudo} class="text-end text-danger fs-6"><span class="material-symbols-outlined">delete</span></a>              
+                {lista.map((atividade) => (
+                    <div key={atividade.id} class="col-sm-4 mb-2">
+                        <div class="card">
+                            <div class="card-body">
+                                <h4 class="card-title">{atividade.atividade}</h4>
+                                <button type="button" class="btn-close m-2" aria-label="Close" onClick={() => remove(atividade.id)}></button>
+                            </div>
+                        </div>
+                    </div>
+                ))}
                 </div>
-            ))}
+            </div> 
         </div>
     );
 }
