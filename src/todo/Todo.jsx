611180eda00c98386918646801488bdce116ed13
Victor de Souza Solique <99771825+VictorSolique@users.noreply.github.com>
2023-09-07 18:46:06 -0400

    commit 07/09

--- src/todo/Todo.jsx
+++ src/todo/Todo.jsx
@@ -1,64 +1,164 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
 
+const listaImg = [
+    {
+        nome: "Pão Francês",
+        url: "https://cdn.pixabay.com/photo/2018/06/10/20/30/bread-3467243_640.jpg",
+        preco: parseFloat(4.5).toFixed(2)
+    },
+    {
+        nome: "Feijão",
+        url: "https://media.istockphoto.com/id/1361420112/photo/carioca-bean-texture-background.webp?b=1&s=612x612&w=0&k=20&c=g9Ams4PfwOl9ohhevA9YjMntELb_8iD5Tf0gjX_VIHs=",
+        preco: parseFloat(5.85).toFixed(2)
+    },
+    {
+        nome: "Arroz",
+        url: "https://media.istockphoto.com/id/153737841/photo/rice.webp?b=1&s=612x612&w=0&k=20&c=OiBx6RbgtHr7by4gcILMoTveSLnVVR7FwHItIz1brtY=",
+        preco: parseFloat(14.5).toFixed(2)
+    },
+    {
+        nome: "Banana",
+        url: "https://cdn.pixabay.com/photo/2018/09/24/20/12/bananas-3700718_640.jpg",
+        preco: parseFloat(8.99).toFixed(2)
+    },
+    {
+        nome: "Farinha de Trigo",
+        url: "https://cdn.pixabay.com/photo/2015/01/14/19/20/bake-599521_640.jpg",
+        preco: parseFloat(14.99).toFixed(2)
+    },
+    {
+        nome: "Açúcar",
+        url: "https://media.istockphoto.com/id/1390646099/photo/a-sugar-spoon.webp?b=1&s=612x612&w=0&k=20&c=wdM9ukqXs9R2ibYYybEa1rZCQ-l5SuY6PeSeQX_Xgg4=",
+        preco: parseFloat(11.49).toFixed(2)
+    },
+    {
+        nome: "Café",
+        url: "https://cdn.pixabay.com/photo/2017/09/04/18/39/coffee-2714970_640.jpg",
+        preco: parseFloat(16.5).toFixed(2)
+    },
+    {
+        nome: "Leite Integral",
+        url: "https://media.istockphoto.com/id/518843282/photo/pouring-milk.jpg?s=612x612&w=0&k=20&c=IVxPvLQ5qMMsz6ach_q-LGeniIpa3KwrPP6bcCIdiSs=",
+        preco: parseFloat(6.79).toFixed(2)
+    },
+    {
+        nome: "Cenoura",
+        url: "https://cdn.pixabay.com/photo/2016/07/11/00/18/carrots-1508847_640.jpg",
+        preco: parseFloat(11.99).toFixed(2)
+    }
+    
+]
 
 export default function Todo() {
     const[lista, setLista] = useState([]);
-    const[atividade, setAtiv] = useState("");
+    const[atividade, setAtiv] = useState(""); 
     const[id, setId] = useState(Math.random());
+    const[preco, setPreco] = useState("");
+
+    const adionaListaImg = (nome, index) => {
+        let inputPreco = document.getElementById("inputPreco " + nome).value;
+            setLista([...lista, {
+                id: index,
+                atividade: nome,
+                preco: Number(inputPreco)
+            }])
+            setId(index+1);
+            console.log(lista);
+    }
 
     const salvar = (e) => {
         e.preventDefault();
-        if(atividade === "") return;
+        if(atividade === "" || preco === "") return;
         setLista([...lista, {
             id: id,
-            atividade: atividade.charAt(0).toUpperCase() + atividade.slice(1)
+            atividade: atividade,
+            preco: Number(preco)
         }]);
         setId(id+1);
         setAtiv("");
+        setPreco('');
     }
     function remove(id) {
         const listaAux = [...lista];
         const filtra = listaAux.filter(a => (id !== a.id ? lista : null));
         setLista(filtra);
-        console.log(filtra, listaAux, id, "Está rondando");  
+        console.log("Removeu compra. Código: " + id);  
     }
     function removeTudo(e) {
         e.preventDefault();
         return setLista([]);
     }
-    
 
     return (
-        <div class="container bg-cinza pt-4">
+        <div className="container bg-cinza pt-4">
             <nav aria-label="breadcrumb">
-                <ol class="breadcrumb">
-                    <li class="breadcrumb-item"><Link to="/">Home</Link></li>
-                    <li class="breadcrumb-item active" aria-current="page">toDo</li>
+                <ol className="breadcrumb">
+                    <li className="breadcrumb-item"><Link to="/">Home</Link></li>
+                    <li className="breadcrumb-item active" aria-current="page">toDo</li>
                 </ol>
             </nav>
+
+            <h1 className="text-start">Lista de Compra de Supermercado</h1>   
             
-            <h1 class="text-start">Lista de Atividades</h1>
-            <form onSubmit={salvar} class="input-group mb-3">
-                <input type="text" class="form-control" placeholder="Adicione uma comida na lista" aria-describedby="button-addon2" value={atividade} onChange={(e) => setAtiv(e.target.value)} />
-                <button class="btn btn-success">ADICIONAR</button>
-            </form>
-            <div class="container">
-                
-                <div class="row">    
-                <a onClick={removeTudo} class="text-end text-danger fs-6"><span class="material-symbols-outlined">delete</span></a>              
-                {lista.map((atividade) => (
-                    <div key={atividade.id} class="col-sm-4 mb-2">
-                        <div class="card">
-                            <div class="card-body">
-                                <h4 class="card-title">{atividade.atividade}</h4>
-                                <button type="button" class="btn-close m-2" aria-label="Close" onClick={() => remove(atividade.id)}></button>
-                            </div>
+            <form onSubmit={salvar} className="row">
+                <div className="col mb-2">
+                    <input type="text" className="form-control" placeholder="Adicione uma compra na lista" value={atividade} onChange={(e) => setAtiv(e.target.value)}/>
+                </div>
+                <div className="col-md-auto mb-2">
+                    <input type="number" step="any" min={0} className="form-control" placeholder="R$ 0,00" value={preco} onChange={(e) => setPreco(e.target.value)} 
+                    />
+                </div>
+                <div className="col col-lg-2">
+                    <button className="btn btn-success" type="submit">ADICIONAR</button>
+                </div>
+            </form>         
+            <div className="container mt-4">
+                <div className="row">
+                    <div className="col p-2 fonte-phudu">
+                    <a onClick={removeTudo} className="under text-danger fs-6" href=""><span className="material-symbols-outlined align-bottom">delete</span>apagar tudo</a>      
+                        <div className="card">
+                            <div className="card-body">   
+                                <h6 className="border-bottom text-center">Lista de Compra</h6>                                
+                            {lista.map((atividade) => (
+                                <div key={atividade.id} className="d-flex justify-content-between border-bottom mb-3">
+                                    <div className="me-auto pe-2">
+                                        <p className="m-0"> - {atividade.atividade}</p>
+                                        <p className="text-danger d-inline"> --&gt; R$</p>
+                                        <input type="number" min="0" className="border-0 text-danger" placeholder="0,00" value={atividade.preco.toFixed(2)} />
+                                    </div>
+                                    <button type="button" className="btn-close mx-1 mt-1 " aria-label="Close" onClick={() => remove(atividade.id)}></button> 
+                                </div>
+                            ))}
+                            </div> 
                         </div>
                     </div>
-                ))}
-                </div>
-            </div> 
+                    <div className="col p-2">    
+                        <div className="row">
+                            <h4 className="fw-light pb-0 mb-0">mais populares</h4>
+                            
+                            {listaImg.map((item) => (
+                                <div className="col-sm-4 mb-2">
+                                    <div className="card">
+                                        <img src={item.url} className="card-img-top" alt={item.nome} />
+                                        <div className="card-body px-0 text-center">
+                                            <p className="card-title">{item.nome}</p>
+                                            <div className="input-group mb-3 px-2">
+                                                <span className="input-group-text border-0" id="basic-addon1">R$</span>
+                                                <input type="number" min={0} className="form-control border-0" placeholder="0,00" defaultValue={item.preco} id={"inputPreco "+ item.nome} />
+                                            </div>
+                                            <button type="button" className="btn btn-success rounded-5 btn-sm fs-6" onClick={(e) => e.preventDefault(adionaListaImg(item.nome, Math.random()))}>Adicionar <span className="material-symbols-outlined align-bottom">add_circle</span></button> 
+                                        </div>
+                                    </div>
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                </div> 
+            </div>
         </div>
     );
 }
+
+
+
